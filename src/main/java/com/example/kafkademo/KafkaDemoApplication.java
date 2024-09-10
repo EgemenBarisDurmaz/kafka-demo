177bd446ac42a794eaf0965816be95aa3aeb6419
ebd <egemenbdurmaz@gmail.com>
2024-09-10 16:54:18 +0200

    Application Entry Point

--- src/main/java/com/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.kafkademo;
 
+import com.example.kafkademo.producer.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaDemoApplication {
+public class KafkaDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private KafkaProducer producer;
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        producer.sendMessage("Hello Kafka");
+    }
 }
