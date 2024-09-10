8a2e6fbfb0b3d723648513f68afd84ad88140288
ebd <egemenbdurmaz@gmail.com>
2024-09-10 16:50:57 +0200

    Consumer created

--- src/main/java/com/example/kafkademo/consumer/KafkaConsumer.java
+++ src/main/java/com/example/kafkademo/consumer/KafkaConsumer.java
@@ -1,4 +1,13 @@
 package com.example.kafkademo.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaConsumer {
+
+        @KafkaListener(topics = "my_topic", groupId = "group_id")
+        public void consume(String message) {
+            System.out.println("Consumed message is: " + message);
+        }
 }
