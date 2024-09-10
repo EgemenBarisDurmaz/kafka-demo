d5820e8d87de8ab4f547c94b8d8c5d8700e7d509
ebd <egemenbdurmaz@gmail.com>
2024-09-10 16:47:07 +0200

    Producer created

--- src/main/java/com/example/kafkademo/producer/KafkaProducer.java
+++ src/main/java/com/example/kafkademo/producer/KafkaProducer.java
@@ -1,4 +1,20 @@
 package com.example.kafkademo.producer;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.print.DocFlavor;
+
+@Service
 public class KafkaProducer {
+
+    private static final String TOPIC = "my-topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+    }
 }
